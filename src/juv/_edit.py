5c569f645bfbef6d9b537b975ea0092847cd237e
Trevor Manz <trevor.j.manz@gmail.com>
2024-10-29 23:10:34 -0400

    Apply smarter diff

--- src/juv/_edit.py
+++ src/juv/_edit.py
@@ -1,3 +1,5 @@
+import difflib
+import operator
 import subprocess
 import tempfile
 from pathlib import Path
@@ -61,15 +63,50 @@ def edit(path: Path, editor: str) -> None:
     text = open_editor(cat(prev_notebook, script=False), suffix=".md", editor=editor)
     new_notebook = jupytext.reads(text.strip(), fmt="md")
 
-    for prev, new in zip(prev_notebook["cells"], new_notebook["cells"]):
+    apply_diff(prev_notebook["cells"], new_notebook["cells"])
+
+    # replace the original notebook cells with the modified ones
+    prev_notebook["cells"] = new_notebook["cells"]
+    path.write_text(jupytext.writes(prev_notebook, fmt="ipynb"))
+
+
+def apply_diff(
+    prev_cells: list[dict], new_cells: list[dict], min_similarity: float = 0.8
+) -> None:
+    """Intelligently diff and merge cells to minimize changes.
+
+    Args:
+        prev_cells: Original notebook cells
+        new_cells: Modified notebook cells
+    Returns:
+        List of merged cells with minimal changes.
+
+    """
+    prev_cells = prev_cells.copy()
+
+    def update(prev: dict, new: dict) -> None:
         if "id" in prev:
             new["id"] = prev["id"]
-
         if "outputs" in prev:
             new["outputs"] = prev["outputs"]
-
         if "execution_count" in prev:
             new["execution_count"] = prev["execution_count"]
 
-    prev_notebook["cells"] = new_notebook["cells"]
-    path.write_text(jupytext.writes(prev_notebook, fmt="ipynb"))
+    for new in new_cells:
+        scores: list[tuple[float, dict]] = []
+        for prev in prev_cells:
+            matcher = difflib.SequenceMatcher(
+                isjunk=None, a="".join(new["source"]), b="".join(prev["source"])
+            )
+            score = matcher.ratio()
+            if score == 1.0:
+                # we have an exact match and can skip the rest
+                prev_cells.remove(prev)
+                update(prev, new)
+                break
+            scores.append((score, prev))
+        if scores:
+            score, prev = max(scores, key=operator.itemgetter(0))
+            if score > min_similarity:  # only update if similarity is high enough
+                prev_cells.remove(prev)
+                update(prev, new)
