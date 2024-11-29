79fd77fa0f06f8fa362bfd671b85828de81f0328
Aaron Leese <atleese@gmail.com>
2024-11-29 14:39:35 -0600

    add check for windows and dont use os.setsid/getpgid() if running on windows

--- src/juv/_run_replace.py
+++ src/juv/_run_replace.py
@@ -1,21 +1,32 @@
 from __future__ import annotations
 
 import os
+import platform
 import signal
 import subprocess
 import sys
 
 from uv import find_uv_bin
 
+IS_WINDOWS = platform.system()
 
 def run(script: str, args: list[str]) -> None:
-    process = subprocess.Popen(  # noqa: S603
-        [os.fsdecode(find_uv_bin()), *args],
-        stdin=subprocess.PIPE,
-        stdout=sys.stdout,
-        stderr=sys.stderr,
-        preexec_fn=os.setsid,  # noqa: PLW1509
-    )
+    if not IS_WINDOWS:
+        process = subprocess.Popen(  # noqa: S603
+            [os.fsdecode(find_uv_bin()), *args],
+            stdin=subprocess.PIPE,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            preexec_fn=os.setsid,  # noqa: PLW1509
+        )
+    else:
+        process = subprocess.Popen(  # noqa: S603
+            [os.fsdecode(find_uv_bin()), *args],
+            stdin=subprocess.PIPE,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            creationflags=subprocess.CREATE_NEW_PROCESS_GROUP
+        ) 
 
     assert process.stdin is not None  # noqa: S101
     process.stdin.write(script.encode())
@@ -25,6 +36,9 @@ def run(script: str, args: list[str]) -> None:
     try:
         process.wait()
     except KeyboardInterrupt:
-        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        if not IS_WINDOWS:
+            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        else:
+            os.kill(process.pid, signal.SIGTERM)
     finally:
         process.wait()
