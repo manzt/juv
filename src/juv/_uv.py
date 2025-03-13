2bb56af735d6556a49ba2e487a0489f837f2d8ab
Trevor Manz <trevor.j.manz@gmail.com>
2025-03-12 22:22:53 -0400

    Make output message nicer

--- src/juv/_uv.py
+++ src/juv/_uv.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import os
 import subprocess
-import sys
 
 from uv import find_uv_bin
 
@@ -35,44 +34,4 @@ def uv(
     uv = os.fsdecode(find_uv_bin())
     return subprocess.run(  # noqa: S603
         [uv, *args], capture_output=True, check=check, env=env or os.environ
-    )
-
-
-def uv_piped(
-    args: list[str],
-    *,
-    check: bool,
-    env: dict | None = None,
-    input: bytes | None = None,  # noqa: A002
-) -> subprocess.CompletedProcess:
-    """Invoke a uv subprocess and stream the output to the console.
-
-    Parameters
-    ----------
-    args : list[str]
-        The arguments to pass to the subprocess.
-
-    check : bool
-        Whether to raise an exception if the subprocess returns a non-zero exit code.
-
-    env : dict | None
-        The system enviroment to run the subprocess.
-
-    input : bytes | None
-        Contents to forwads as input to the subprocess.
-
-    Returns
-    -------
-    subprocess.CompletedProcess
-        The result of the subprocess.
-
-    """
-    uv = os.fsdecode(find_uv_bin())
-    return subprocess.run(  # noqa: S603
-        [uv, *args],
-        check=check,
-        env=env or os.environ,
-        stdout=sys.stdout,
-        stderr=sys.stderr,
-        input=input,
     )
--- src/juv/_venv.py
+++ src/juv/_venv.py
@@ -6,8 +6,10 @@ import typing
 from pathlib import Path
 
 import jupytext
+import rich
+from rich.console import Console
 
-from juv._uv import uv, uv_piped
+from juv._uv import uv
 
 from ._nbutils import code_cell, write_ipynb
 from ._pep723 import includes_inline_metadata
@@ -23,13 +25,13 @@ def sync(
     python: str | None,
     frozen: bool = False,
     env_path: pathlib.Path,
-) -> None:
+) -> str:
     env = os.environ.copy()
     env["VIRTUAL_ENV"] = str(env_path)
 
     if path.suffix == ".py":
         # just defer to uv behavior
-        uv_piped(
+        result = uv(
             [
                 "sync",
                 *(["--python", python] if python else []),
@@ -40,7 +42,7 @@ def sync(
             check=True,
             env=env,
         )
-        return
+        return result.stderr.decode("utf8")
 
     notebook = jupytext.read(path, fmt="ipynb")
     lockfile_contents = notebook.get("metadata", {}).get("uv.lock")
@@ -73,7 +75,7 @@ def sync(
         if lockfile_contents:
             lockfile.write_text(lockfile_contents)
 
-        uv_piped(
+        result = uv(
             [
                 "sync",
                 *(["--python", python] if python else []),
@@ -90,6 +92,8 @@ def sync(
             write_ipynb(notebook, path)
             lockfile.unlink(missing_ok=True)
 
+        return result.stderr.decode("utf8")
+
 
 def venv(
     *,
@@ -98,8 +102,63 @@ def venv(
     path: pathlib.Path,
     no_kernel: bool,
 ) -> None:
-    sync(source, python=python, env_path=path)
+    console = Console()
+    rel_path = os.path.relpath(path.resolve(), Path.cwd())
+    if path.exists():
+        rich.print(f"Using notebook environment at: `[cyan]{rel_path}[/cyan]`")
+    else:
+        rich.print(f"Creating notebook environment at: `[cyan]{rel_path}[/cyan]`")
+
+    uv_output = sync(source, python=python, env_path=path)
     env = os.environ.copy()
     env["VIRTUAL_ENV"] = str(path)
     if not no_kernel:
         uv(["pip", "install", "ipykernel"], env=env, check=True)
+
+    for line in uv_output.split("\n"):
+        if line.startswith((" +", " -", " ~")):
+            prefix, suffix = line.split("==")
+            symbol, name = prefix.strip().split(" ")
+            color = {"+": "green", "-": "red", "~": "yellow"}[symbol]
+            if name in IGNORE_PACKAGES:
+                continue
+            console.print(
+                f" [{color}]{symbol}[/{color}] [bold]{name}[/bold][dim]=={suffix}[/dim]",  # noqa: E501
+                highlight=False,
+            )
+
+
+# These packages are dependencies of `ipykernel` so they add a lot of noise to output.
+# We filter them out when reporting differences in the environment.
+IGNORE_PACKAGES = {
+    "appnope",
+    "asttokens",
+    "comm",
+    "debugpy",
+    "decorator",
+    "executing",
+    "ipykernel",
+    "ipython",
+    "ipython-pygments-lexers",
+    "jedi",
+    "jupyter-client",
+    "jupyter-core",
+    "matplotlib-inline",
+    "nest-asyncio",
+    "packaging",
+    "parso",
+    "pexpect",
+    "platformdirs",
+    "prompt-toolkit",
+    "psutil",
+    "ptyprocess",
+    "pure-eval",
+    "pygments",
+    "python-dateutil",
+    "pyzmq",
+    "six",
+    "stack-data",
+    "tornado",
+    "traitlets",
+    "wcwidth",
+}
