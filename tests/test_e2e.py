88fab5f4f095114e1ca39919684bc3a8486bb9ec
Trevor Manz <trevor.j.manz@gmail.com>
2025-03-19 16:37:44 -0400

    Wait for server output

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -4,8 +4,6 @@ import pathlib
 import subprocess
 import time
 import typing
-import urllib.error
-import urllib.request
 
 import pytest
 
@@ -19,21 +17,27 @@ SELF_DIR = pathlib.Path(__file__).parent
 ROOT = SELF_DIR / ".."
 
 
-def juv(args: list[str], *, wait_and_check: bool = True) -> subprocess.Popen:
-    process = subprocess.Popen(["uv", "run", "juv", *args], cwd=ROOT)  # noqa: S603, S607
+@pytest.fixture(autouse=True)
+def notebook() -> typing.Generator[pathlib.Path]:
+    path = (ROOT / "smoke.ipynb").resolve()
+    yield path
+    path.unlink(missing_ok=True)
+
+
+def juv(args: list[str], *, wait_and_check: bool = True) -> subprocess.Popen[str]:
+    process = subprocess.Popen(  # noqa: S603
+        ["uv", "run", "juv", *args],  # noqa: S607
+        cwd=ROOT,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True,
+    )
     if wait_and_check:
         exit_code = process.wait(2)
         if exit_code != 0:
             msg = f"juv command failed: {args}, exit code: {exit_code}"
             raise RuntimeError(msg)
     return process
-
-
-@pytest.fixture(autouse=True)
-def notebook() -> typing.Generator[pathlib.Path]:
-    path = (ROOT / "smoke.ipynb").resolve()
-    yield path
-    path.unlink(missing_ok=True)
 
 
 def test_juv_run(page: Page, notebook: pathlib.Path) -> None:
@@ -52,28 +56,17 @@ def test_juv_run(page: Page, notebook: pathlib.Path) -> None:
         ],
         wait_and_check=False,
     )
+    # FIXME: nicer way to wait for the server to start  # noqa: FIX001, TD001, TD002, TD003
+    time.sleep(2)
+    if process.poll() is not None:
+        msg = "OH NO" if process.stdout is None else process.stdout.readlines()
+        raise ValueError(msg)
     url = "http://127.0.0.1:8888/lab"
-    wait_for_webserver(url)
     page.goto(url)
     # Menu
     page.get_by_text("File", exact=True).click()
     page.get_by_role("menu").get_by_text("Shut Down", exact=True).click()
     # Modal
     page.get_by_role("button", name="Shut Down").click()
-    process.wait(10)
+    process.wait()
     assert notebook.exists()
-
-
-def wait_for_webserver(url: str, timeout: int = 10) -> None:
-    """Wait for webserver to be available"""
-    start_time = time.time()
-    while time.time() - start_time < timeout:
-        try:
-            with urllib.request.urlopen(url) as response:  # noqa: S310
-                if response.status == 200:  # noqa: PLR2004
-                    return
-        except (urllib.error.URLError, urllib.error.HTTPError):
-            pass
-        time.sleep(0.2)
-    msg = "web server did not start in time"
-    raise RuntimeError(msg)
